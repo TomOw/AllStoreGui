fbd364943be4e64b47885ab46c828be30402e87a
TomOw <owczarczyk.tomasz@wp.pl>
2016-11-15 21:36:15 +0100

    added theme configuration, css layout files, layout changes in HTMLs

---
+++ js/controller/ItemFormController.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Tomasz on 08.11.2016.
+ */
+
+app.controller('ItemFormController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.test = 'testMsg';
+
+    $scope.getStore = function () {
+        $http.get('http://localhost:8080/store/eigth')
+            .success(function (response, status) {
+                console.log('z geta' + response.toString());
+                console.log('z geta' + response);
+                console.log(response);
+                $scope.store = response;
+                //$scope.raw = data;
+            })
+    };
+
+    $scope.store = {
+        id: 0,
+        name: '',
+        rating: 0.0,
+        noOfOrdersMade: 0,
+        noOfRates: 0,
+        storeAddress: {
+            id: 0,
+            country: '',
+            city: '',
+            street: '',
+            number: '',
+            postalCode: ''
+        },
+        items: [],
+        orders: []
+    };
+
+    $scope.smth = 'gasg';
+
+}]);
