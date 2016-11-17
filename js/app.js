684066c91093c0035c3db3e16a77d681ced46578
TomOw <owczarczyk.tomasz@wp.pl>
2016-11-17 23:25:36 +0100

    routing example done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
-var app = angular.module('BlankApp', ['ngMaterial'])
+var app = angular.module('BlankApp', ['ngMaterial', 'ngRoute'])
     .config(function($mdThemingProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('teal')
             .accentPalette('red')
             .warnPalette('red');
     });
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'templates/first.html'
+        })
+        .when('/second', {
+            templateUrl: 'templates/second.html'
+        })
+        .otherwise({
+            template: '<h1>otherwise template</h1>'
+        })
+})
---
+++ js/controller/sampleController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Tomasz on 17.11.2016.
+ */
+app.controller('sampleController', ['$scope', function ($scope) {
+
+
+    $scope.sample = 'sampleMSG';
+
+}]);
