386516847c8be85b2fcb3d12e18efb84c73b2c95
TomOw <owczarczyk.tomasz@wp.pl>
2016-11-20 22:03:04 +0100

    added list of offers to single item view

--- js/controller/ItemController.js
+++ js/controller/ItemController.js
@@ -3,6 +3,8 @@
  */
 app.controller('ItemController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
 
+    var url = 'http://localhost:8080';
+
     $scope.fooMsg = 'sample message to test something';
 
     var itemId = $routeParams.itemId;
@@ -14,6 +16,20 @@ app.controller('ItemController', ['$scope', '$http', '$routeParams', function ($
                 $scope.single = response;
                 console.log('response: ' + JSON.stringify(response.data));
                 console.log('scope: ' + $scope.item);
+            })
+    };
+
+    $scope.getCheapestOffer = function () {
+        $http.get('http://localhost:8080/item/cheapest/' + $scope.single.name)
+            .success(function (response, status) {
+                $scope.cheapest = response;
+            })
+    };
+
+    $scope.getOffers = function () {
+        $http.get(url + '/item/offers/' + $scope.single.name)
+            .success(function (response, status) {
+                $scope.offers = response;
             })
     };
 
---
+++ js/service/StoreService.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Tomasz on 20.11.2016.
+ */
+app.service('StoreService', ['$http', function ($http) {
+
+}]);
