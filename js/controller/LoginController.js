4eb4e25865b78d1f7e600ec91015a3eafa3ea0a7
TomOw <owczarczyk.tomasz@wp.pl>
2017-01-26 22:45:28 +0100

    styled ul and li, added info dialog at login page

--- js/controller/LoginController.js
+++ js/controller/LoginController.js
@@ -1,4 +1,4 @@
-app.controller('LoginController', function($rootScope, $scope, AuthSharedService) {
+app.controller('LoginController', function($rootScope, $scope, $mdDialog, $location, AuthSharedService) {
 
 
     $scope.rememberMe = true;
@@ -7,5 +7,22 @@ app.controller('LoginController', function($rootScope, $scope, AuthSharedService
     $scope.login = function() {
         $rootScope.authenticationError = false;
         AuthSharedService.login($scope.username, $scope.password, $scope.rememberMe);
-    }
+    };
+
+    $scope.infoDialog = function (ev) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+            .title('Sample credentials')
+            .textContent('Sample credentials can be found at about section')
+            .ariaLabel('Lucky day')
+            .targetEvent(ev)
+            .ok('OK')
+            .cancel('About page');
+
+        $mdDialog.show(confirm).then(function () {
+        }, function () {
+            console.log('called redirect to info page');
+            $location.path('/info');
+        });
+    };
 });
